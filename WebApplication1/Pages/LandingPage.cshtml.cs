141b5c835a56a1c8a35bb0578a27ca52da0a9b61
Thomas <thefeardjewel@gmail.com>
2023-11-30 14:16:49 +0100

    trying to get images to display

--- WebApplication1/Pages/LandingPage.cshtml.cs
+++ WebApplication1/Pages/LandingPage.cshtml.cs
@@ -7,10 +7,10 @@ namespace WebApplication1.Pages
     public class LandingPageModel : PageModel 
     {
         [BindProperty]
-        public List<Movie> movies {  get; set; }
+        public List<Movie> Movies {  get; set; }
         public void OnGet()
         {
-            movies = new List<Movie>() { new Movie { Id = 0, ImageUrl = ""}, new Movie { Id = 1, ImageUrl = "Avatar-The-Way-of-Water.jpg" }, new Movie { Id = 2, ImageUrl = "" }, new Movie { Id = 3, ImageUrl = "" }, new Movie { Id = 4, ImageUrl = "" } };
+            Movies = new List<Movie>() { new Movie { Id = 0, ImageUrl = ""}, new Movie { Id = 1, ImageUrl = "Avatar-The-Way-of-Water.jpg" }, new Movie { Id = 2, ImageUrl = "" }, new Movie { Id = 3, ImageUrl = "" }, new Movie { Id = 4, ImageUrl = "" } };
         }
     }
 }
