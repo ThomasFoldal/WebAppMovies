aa9d7f3b30b59775798c40042f7b5c61114e50aa
Thomas Foldal Jakobsen <thom196c@edu.sde.dk>
2023-12-14 23:54:21 +0100

    V1

--- WebApplication1/Models/Movie.cs
+++ WebApplication1/Models/Movie.cs
@@ -5,7 +5,7 @@
         public int Id { get; set; }
         public string ImageUrl { get; set; }
         public string Title { get; set; }
-        public List<string>? Categories { get; set; } = null;
+        //public List<string>? Categories { get; set; } = null;
         public string? Summery { get; set; }
     }
 }
--- WebApplication1/Pages/LandingPage.cshtml.cs
+++ WebApplication1/Pages/LandingPage.cshtml.cs
@@ -4,10 +4,10 @@ using WebApplication1.Models;
 
 namespace WebApplication1.Pages
 {
-    public class LandingPageModel : PageModel 
+    public class LandingPageModel : PageModel
     {
         [BindProperty]
-        public List<Movie> Movies {  get; set; }
+        public List<Movie> Movies { get; set; }
         private readonly MyDatabaseContext _dbContext;
         public LandingPageModel(MyDatabaseContext databasecontext)
         {
@@ -16,14 +16,21 @@ namespace WebApplication1.Pages
 
         public void OnGet()
         {
-            Movies = new List<Movie>() { 
-                new Movie { Id = 0, ImageUrl = "/Images/avatar.jpg", Title = "Avatar the way of water", Summery = "fsdjfgasjglækasjdlgan<sgjs", Categories = null}, 
-                new Movie { Id = 1, ImageUrl = "/Images/BackToTheFuture.jpg", Title = "Back to the future", Summery = "vgnæakjfngklasjdfnblæajdnfgh", Categories = null}, 
-                new Movie { Id = 2, ImageUrl = "/Images/Barbie.jpg", Title = "Barbie", Summery = "faklsjdhglkajfghaijfsdgh", Categories = null}, 
-                new Movie { Id = 3, ImageUrl = "/Images/Oppenheimer.png", Title = "Oppenheimer", Summery = "pdnfbkiahdfkbjnadfg", Categories = null}, 
-                new Movie { Id = 4, ImageUrl = "/Images/Saw.jpg", Title = "Saw", Summery = "sdfgihadfiguhasepirog", Categories = null} };
-            _dbContext.Add(Movies);
-            _dbContext.SaveChanges();
+            if (_dbContext.DBMovies.Count() == 0)
+            {
+
+                Movies = new List<Movie>() {
+                new Movie { Id = 1, ImageUrl = "/Images/BackToTheFuture.jpg", Title = "Back to the future", Summery = "vgnæakjfngklasjdfnblæajdnfgh"},
+                new Movie { Id = 2, ImageUrl = "/Images/Barbie.jpg", Title = "Barbie", Summery = "faklsjdhglkajfghaijfsdgh"},
+                new Movie { Id = 3, ImageUrl = "/Images/Oppenheimer.png", Title = "Oppenheimer", Summery = "pdnfbkiahdfkbjnadfg"},
+                new Movie { Id = 4, ImageUrl = "/Images/Saw.jpg", Title = "Saw", Summery = "sdfgihadfiguhasepirog"},
+                new Movie { Id = 5, ImageUrl = "/Images/avatar.jpg", Title = "Avatar the way of water", Summery = "fsdjfgasjglækasjdlgan<sgjs"}};
+                foreach (Movie m in Movies)
+                {
+                    _dbContext.Add(m);
+                }
+                _dbContext.SaveChanges();
+            }
 
             Movies = _dbContext.DBMovies.Take(5).ToList();
 
